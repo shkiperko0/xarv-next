fa0abb525d398a87baf7ea558d64eec044d99a0c
Roman Adamov <shkiperko0@gmail.com>
2023-05-09 00:28:10 +0600

    initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import statics from '@/statics'
+import styles from 'styles/Home.module.css'
+import statics from 'src/statics'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
-  
-console.log(statics.mode)
-
   return (
     <>
       <Head>
