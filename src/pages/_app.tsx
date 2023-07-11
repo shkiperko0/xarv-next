7c466222a3830ffd82db9afdd313ff3a855c8da3
Roman Adamov <shkiperko0@gmail.com>
2023-07-12 01:27:46 +0600

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'src/styles/globals.css'
+import 'src/globals.css'
 import type { AppProps } from 'next/app'
-import { WindowsLayout } from 'src/xarv/modules/taskmanager/index'
+import { WindowsLayout } from 'shared/xarv/modules/taskmanager'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
